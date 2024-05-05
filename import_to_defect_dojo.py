dabfb59dd72a09fec9cec26d3abf8ec1581ef2c2
hoainamnv34 <hoainam.nv34@gmail.com>
2024-05-05 13:01:44 +0700

    update

--- import_to_defect_dojo.py
+++ import_to_defect_dojo.py
@@ -1,15 +1,25 @@
+import string
 import requests
 import sys
 import os
 
 
 def uploadToDefectDojo(is_new_import, token, url, product_name, engagement_name, scan_type, filename):
-    multipart_form_data = {
-        'file': (filename, open(filename, 'rb')),
-        'scan_type': (None, scan_type),
-        'product_name': (None, product_name),
-        'engagement_name': (None, engagement_name),
-    }
+
+    if filename == '':
+        multipart_form_data = {
+            'scan_type': (None, scan_type),
+            'product_name': (None, product_name),
+            'engagement_name': (None, engagement_name),
+        }
+
+    else:
+        multipart_form_data = {
+            'file': (filename, open(filename, 'rb')),
+            'scan_type': (None, scan_type),
+            'product_name': (None, product_name),
+            'engagement_name': (None, engagement_name),
+        }
 
     endpoint = '/api/v2/import-scan/' if is_new_import else '/api/v2/reimport-scan/'
     r = requests.post(
@@ -45,10 +55,10 @@ if __name__ == "__main__":
         is_new_import = True if is_new_import=="True" else False
 
         print("token ", token, "host: ", url, " product_name", product_name, " engagement_name", engagement_name, " scan_type", scan_type)
-
+        
         uploadToDefectDojo(is_new_import, token, url, product_name, engagement_name, scan_type, report)
     else:
         print(
-            'Usage: python3 import_semgrep_to_defect_dojo.py --host DOJO_URL --product PRODUCT_NAME --engagement ENGAGEMENT_NAME --report REPORT_FILE --new-import True/False')
+            'Usage: python3 import_to_defect_dojo.py --host DOJO_URL --product PRODUCT_NAME --engagement ENGAGEMENT_NAME --report REPORT_FILE --new-import True/False')
     
     
--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #python3 import_semgrep_to_defect_dojo.py --host "http://localhost:8080" --product "WebGoat App" --engagement "Initial Security test" --scan "Gitleaks Scan" --report ./report/gitleak-report/report.json
-python3 import_semgrep_to_defect_dojo.py --host "http://localhost:8080" --product "WebGoat App" --engagement "Scan 2" --scan "Gitleaks Scan" --report ./report/gitleak-report/report.json
+python3 import_to_defect_dojo.py --host "http://localhost:8080" --product "WebGoat App" --engagement "Scan 2" --scan "SonarQube API Import" --report '' --new True --token 0026e57d77b876b033736e9644e4e54ed41aa7f7
